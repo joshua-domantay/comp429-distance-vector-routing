b3c8c4027fc0c0169cc8ecbd1810a5a36929515f
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2023-04-13 00:27:56 -0700

    Handle inputs from user. Add help command. Add topology file

--- server.py
+++ server.py
@@ -7,14 +7,104 @@ import sys
 import os
 import argparse
 
-# Help command
+def check_server_id_errors(server_id):
+    if not server_id.isdigit():
+        return 2
+
+    # TODO: Check if server_id is in topology
+    # if
+    # return 1
+
+    return 0
+    
+def check_server_id(server_id):
+    error = check_server_id_errors(server_id)
+    if error == 2:
+        print("Server id \"" + server_id + "\" is not a digit")
+    elif error == 1:
+        print("Cannot find server id \"" + server_id + "\"")
+    else:
+        return True
+
+# Command help
 def list_commands():
-    pass
+    print("update <server_id1> <server_id2> <link_cost>")
+    print("  > Set the link cost between server_id1 and server_id2 to link_cost. Link cost must be a positive integer or infinity (inf)")
+    print("step")
+    print("  > Send routing update to neighbors")
+    print("packets")
+    print("  > Display the number of distance vector (packets) this server has received since last invocation of this information")
+    print("display")
+    print("  > Display the current routing table, including current and neighboring nodes' distance vector")
+    print("disable <server_id>")
+    print("  > Disable the link to a given server")
+    print("crash")
+    print("  > Close all connections on all links (simulate server crashes)")
+    print()
+
+# Command update
+def update(server_id1, server_id2, link_cost):
+    # Check arguments
+    valid = True
+    if not check_server_id(server_id1):
+        valid = False
+    if not check_server_id(server_id2):
+        valid = False
+    if (not link_cost.isdigit()):
+        print("Link cost must be a positive integer or infinity (inf)")
+        valid = False
+    if not valid:
+        return
+    
+    print("update <server_id1> <server_id2> <link_cost>")
+
+# Command step
+def send_routing_update():
+    print("step")
+
+# Command packets
+def display_packets():
+    print("packets")
+
+# Command display
+def display_routing_table():
+    print("display")
+
+# Command disable
+def disable_server(server_id):
+    if not check_server_id(server_id):
+        return
+    print("disable <server_id>")
+
+# Command crash
+def crash():
+    print("crash")
 
 def handle_input():
     while True:
         user_input = input(">> ")
         user_input = user_input.lower().split(" ")
+        if user_input[0] == "help":
+            list_commands()
+        if user_input[0] == "update":
+            if len(user_input) == 4:
+                update(user_input[1], user_input[2], user_input[3])
+            else:
+                print("Please provide two server ids and link cost. Use \"help\" for more info")
+        if user_input[0] == "step":
+            send_routing_update()
+        if user_input[0] == "packets":
+            display_packets()
+        if user_input[0] == "display":
+            display_routing_table()
+        if user_input[0] == "disable":
+            if len(user_input) == 2:
+                disable_server(user_input[1])
+            else:
+                print("Please provide server id. Use \"help\" for more info")
+        if user_input[0] == "crash":
+            crash()
+        print()
 
 def valid_args(args):
     valid = True    # Instead of returning immediately, use boolean so it prints all errors
@@ -46,7 +136,7 @@ def main(args):
     # Check if argument is missing
     if not valid_args(args):
         return 1
-    print("SUCCESS")
+    handle_input()
     return 0
 
 if __name__ == "__main__":
